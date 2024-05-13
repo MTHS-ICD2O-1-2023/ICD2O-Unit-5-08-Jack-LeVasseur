ecb878cf3d12aec4ba2f50cddc0390592df531fc
Jack LeVasseur <159185068+Jack-LeVasseur@users.noreply.github.com>
2024-05-13 17:51:36 +0000

    Updated index.html

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Jack LeVasseur
+// Created on: March 7 2024
 // This file contains the JS functions for index.html
+
+
+/**
+* Do basic math.
+*/
+function doMathClicked() {
+  document.getElementById("subtract-math").innerHTML = "<p>The area is cm2 =  " + (30 - 15) + "</p>"
+  document.getElementById("multiply-math").innerHTML = "<p>The perimeter is cm = " + (4 + 4 * 2) + "</p>"
+}
